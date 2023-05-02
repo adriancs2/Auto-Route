ba546bf176964eea81c7044e153c412306555ca8
adriancs2 <adriancs202@gmail.com>
2023-05-03 00:24:35 +0800

    update

--- source code/AutoRouteTest/Global.asax.cs
+++ source code/AutoRouteTest/Global.asax.cs
@@ -12,7 +12,15 @@ namespace AutoRouteTest
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            // route all aspx pages in the project
+            // scan through all folders (including folders of javascript, images, css)
             AutoRoute.RouteAllPages();
+
+            // route all aspx pages in specific folder, including sub-folders
+            //AutoRoute.Route("~/pages", true);
+
+            // route all aspx pages in specific folder, do not include pages in sub-folders
+            //AutoRoute.Route("~/pages", false);
         }
 
         protected void Session_Start(object sender, EventArgs e)
